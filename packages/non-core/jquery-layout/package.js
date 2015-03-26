375de82a027a68b4a9039617933369fdcd0c426e
Tom Freudenberg <tom.freudenberg@4commerce.de>
2015-03-26 10:11:13 +0100

    Commented out api.versionsFrom() from non-core jquery-history and jquery-layout package
        this will break meteor dev_bundle installation if non-core packages linked into packages
        see issue https://github.com/meteor/meteor/issues/4023

--- packages/non-core/jquery-layout/package.js
+++ packages/non-core/jquery-layout/package.js
@@ -8,7 +8,12 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom("1.0");
+  // commented out TomFreudenberg 2015-03-26
+  // this will break meteor dev_bundle installation
+  // if non-core packages linked into packages
+  // see issue https://github.com/meteor/meteor/issues/4023
+  // api.versionsFrom('1.0');
+  // ---------------------------------------
   api.use('jquery');
   api.addFiles('jquery.layout.js', 'client');
 });
