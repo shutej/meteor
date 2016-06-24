c38c9e60d4c39cff194fb35a016d6fa044281b67
Tom Freudenberg <tom.freudenberg@4commerce.de>
2016-06-25 01:27:48 +0200

    Re-Add and modify the build-node-for-dev-bundle.sh script when running on ARM
    	add this script again for support of node_dev_bundle on ARM
    	add option --without-snapshot to configure when building node on ARM
    	meteor does not need a forked nodejs but use standard releases

---
+++ scripts/build-node-for-dev-bundle.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+source "$(dirname $0)/build-dev-bundle-common.sh"
+echo CHECKOUT DIR IS "$CHECKOUT_DIR"
+echo BUILDING NODE "v$NODE_VERSION" IN "$DIR"
+
+git clone --branch "v${NODE_VERSION}" --depth 1 \
+    https://github.com/nodejs/node.git
+cd node
+rm -rf .git
+if [ "$METEOR_UNIVERSAL_FLAG" == "ARM" ] ; then
+  ./configure --without-snapshot --prefix="$DIR"
+else
+  ./configure --prefix="$DIR"
+fi
+make -j4
+make install PORTABLE=1
+# PORTABLE=1 is a node hack to make npm look relative to itself instead
+# of hard coding the PREFIX.
+
+cd "$DIR"
+stripBinary bin/node
+
+# export path so we use our new node for later builds
+PATH="$DIR/bin:$PATH"
+which node
+which npm
+
+echo BUNDLING
+
+cd "$DIR"
+rm -rf build
+tar czvf "${CHECKOUT_DIR}/node_${PLATFORM}_v${NODE_VERSION}.tar.gz" .
+
+echo DONE
